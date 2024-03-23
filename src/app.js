10c7f39d654f22ab886e11ae6f0fcbf64553ea85
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2024-03-23 02:51:22 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const suits = ["hearts", "clubs", "diamonds", "spades"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const card = document.querySelector(".card");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomCard = numbers[Math.floor(Math.random() * numbers.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  card.className = `card ${randomSuit}`;
+
+  document.querySelector(".center").textContent = randomCard;
+
+  const suitsElements = document.querySelectorAll(".suit");
+
+  suitsElements.forEach(suitElement => {
+    suitElement.textContent = "";
+    suitElement.classList.remove("hearts", "clubs", "diamonds", "spades");
+    suitElement.classList.add(randomSuit);
+    suitElement.textContent = suitElement.getAttribute("data-suit");
+  });
+}
+
+generateRandomCard();
